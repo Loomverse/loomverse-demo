737a510b84faeed349310007b57e7b24461db398
Salih Khan <52384002+salih-khan@users.noreply.github.com>
2026-03-26 13:16:18 +0000

    sound added

--- script.js
+++ script.js
@@ -5,6 +5,17 @@ var playButton = document.getElementById('play-button');
 
 var isPlaying = false;
 
+// AUDIOS
+var backgroundAudio = document.getElementById('background');
+var deathAudio = document.getElementById('death');
+var gunAudio = document.getElementById('gun');
+var walkingAudio = document.getElementById('walking');
+var warningAudio = document.getElementById('warning');
+var doorAudio = document.getElementById('door');
+var shiveringAudio = document.getElementById('shivering');
+
+
+
 playButton.addEventListener('click', function() {
     isPlaying = true;
     // Here you can add code to transition to the game scene
@@ -33,6 +44,12 @@ function renderScene(scene) {
     sceneDescription.textContent = scene.description;
     sceneImage.src = scene.image;
     choicesContainer.innerHTML = '';
+
+    stopAllAudio();
+
+    if (scene.audio) {
+        scene.audio.play();
+    }
     scene.choices.forEach(function(choice) {
 
         var choiceButton = document.createElement('button');
@@ -49,12 +66,28 @@ function renderScene(scene) {
     });
 }
 
+const allAudio = [
+    backgroundAudio,
+    deathAudio,
+    gunAudio,
+    walkingAudio,
+    warningAudio,
+    doorAudio,
+    shiveringAudio
+];
 
+function stopAllAudio() {
+    allAudio.forEach(function(audio) {
+        audio.pause();
+        audio.currentTime = 0;
+    });
+}
 const gameObject = [{ // END OF ARRAY
             name: 'The Farmhouse',
             id: "first_scene",
             description: 'You find yourself walking down an eerie road. You see a farmhouse in the distance.',
             image: './assets/first_scene.png',
+            audio: backgroundAudio,
             choices: [{
                     text: 'Enter the farmhouse',
                     nextScene: 'house_scene'
@@ -68,54 +101,55 @@ const gameObject = [{ // END OF ARRAY
         {
             name: 'Inside the Farmhouse',
             id: "house_scene",
+            audio: doorAudio,
             description: 'You step inside the farmhouse and find an old yet intact interior. The smell of sulfur fills the air.',
             image: './assets/house_scene.png',
             choices: [{
-                text: 'Go and sleep outside',
-                nextScene: 'blank2'
-            },
-            {
-                text: 'Shoot the thing!',
-                nextScene: 'blank'
-            },
-            {
-                text: 'Make your friendly intentions known',
-                nextScene: 'friendly_intentions'
-            }
-        ]
+                    text: 'Go and sleep outside',
+                    nextScene: 'blank2'
+                },
+                {
+                    text: 'Shoot the thing!',
+                    nextScene: 'blank'
+                },
+                {
+                    text: 'Make your friendly intentions known',
+                    nextScene: 'friendly_intentions'
+                }
+            ]
         },
-        { 
+        {
             name: 'Walking Outside',
             id: "outside_walking",
+            audio: walkingAudio,
             description: 'You find yourself walking down the dark road. You find no option but to continue forth. . .',
             image: './assets/walking_scene.png',
             choices: [{
-                    text: 'Continue walking',
-                    nextScene: 'pathway_scene'
-                }
-            ],
+                text: 'Continue walking',
+                nextScene: 'pathway_scene'
+            }],
         },
-        { 
+        {
             name: 'Dead End',
             id: "pathway_scene",
+            audio: deathAudio,
             description: 'You stumble onto a path where you are ambushed by a scarecrow! You die.',
             image: './assets/pathway_scene.png',
             choices: [{
-                    text: 'Play Again',
-                    nextScene: 'first_scene'
-                }
-            ],
+                text: 'Play Again',
+                nextScene: 'first_scene'
+            }],
         },
-        { 
+        {
             name: 'Friendly Intentions',
             id: "friendly_intentions",
+            audio: warningAudio,
             description: 'He approaches you with inepth curiosity. He allows you to stay the night.',
             image: './assets/friend_scene.jpg',
             choices: [{
-                    text: 'Next',
-                    nextScene: 'win3'
-                }
-            ],
+                text: 'Next',
+                nextScene: 'win3'
+            }],
         },
         {
             name: '',
@@ -123,8 +157,8 @@ const gameObject = [{ // END OF ARRAY
             description: '...',
             image: './assets/win/win3.png',
             choices: [{
-                    text: 'Play Again',
-                    nextScene: 'first_scene'
+                text: 'Play Again',
+                nextScene: 'first_scene'
             }]
         },
         {
@@ -132,9 +166,10 @@ const gameObject = [{ // END OF ARRAY
             id: "blank",
             description: 'You killed a guardian keeping darker things at bay. You survive albeit others that pass through will not due to your actions...',
             image: './assets/black.png',
+            audio: gunAudio,
             choices: [{
-                    text: 'Next',
-                    nextScene: 'win2'
+                text: 'Next',
+                nextScene: 'win2'
             }]
         },
         {
@@ -142,9 +177,10 @@ const gameObject = [{ // END OF ARRAY
             id: "blank2",
             description: 'You took the safest option in all this. Although the night was uncomfortable you are atleast alive!',
             image: './assets/black copy.png',
+            audio: shiveringAudio,
             choices: [{
-                    text: 'Next',
-                    nextScene: 'win1'
+                text: 'Next',
+                nextScene: 'win1'
             }]
         },
         {
@@ -153,18 +189,18 @@ const gameObject = [{ // END OF ARRAY
             description: '',
             image: './assets/win/win2.png',
             choices: [{
-                    text: 'Play Again',
-                    nextScene: 'first_scene'
+                text: 'Play Again',
+                nextScene: 'first_scene'
             }]
         },
         {
-            name: '',
+            name: 'Atleast you are alive..',
             id: "win1",
             description: '',
             image: './assets/win/win1.png',
             choices: [{
-                    text: 'Play Again',
-                    nextScene: 'first_scene'
+                text: 'Play Again',
+                nextScene: 'first_scene'
             }]
         }
     ] // END OF ARRAY
